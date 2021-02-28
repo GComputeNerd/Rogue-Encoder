3d49e4118ab6bed8ba6c4a5a4e243a5734e6dc83
GComputeNerd <GComputeNerd@users.noreply.github.com>
2021-02-28 16:34:13 +0530

    Added test.sh

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# TESTING WHETHER COMMANDS EXIST
+
+if [ -z $(command -v ffmpeg) ]; then
+		echo "ffmpeg not found."
+elif [ -z $(command -v ffprobe) ]; then
+		echo "ffprobe not found."
+else
+		echo "Packages found."
+fi
+
+if [ $(python -c 'import bullet'; echo $?) = 1 ]; then
+		echo "Python Module ''bullet' not found"
+else
+		echo "Python Module Requirements Met."
+fi
